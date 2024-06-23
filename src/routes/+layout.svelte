2b0ae565c7e405dbf6ea6810a772b2fc457c0406
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-23 09:13:40 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class='mt-8 mb-16 p-4'>
+	<h1>Flowbite Svelte Test with Svelte 5</h1>
+	<slot></slot>
+</main>
+
---
+++ src/routes/dropdown/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+  import { ChevronDownOutline } from 'flowbite-svelte-icons'; 
+  import { page } from '$app/stores';
+  $: activeUrl = $page.url.pathname;
+</script>
+
+<h1>Dropdown NOT WORKING</h1>
+<h2>Example</h2>
+<div class='flex'>
+<Button>Dropdown button<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
+<Dropdown>
+  <DropdownItem>Dashboard</DropdownItem>
+  <DropdownItem>Settings</DropdownItem>
+  <DropdownItem>Earnings</DropdownItem>
+  <DropdownItem>Sign out</DropdownItem>
+</Dropdown>
+</div>
+
+<h2>Adding links and active class</h2>
+<div class='flex'>
+  <Button>Dropdown button<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
+<Dropdown {activeUrl}>
+  <DropdownItem href="/">Home</DropdownItem>
+  <DropdownItem href="/dropdown">Dropdown</DropdownItem>
+  <DropdownItem href="/docs/components/accordion">Accordion</DropdownItem>
+  <DropdownItem href="/docs/components/alert">Alert</DropdownItem>
+</Dropdown>
+</div>
