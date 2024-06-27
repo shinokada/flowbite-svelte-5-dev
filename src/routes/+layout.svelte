e027c1ee3529deb248ef50fee11b8c52965582a1
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-27 06:10:49 +0200

    feat: version badges

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
 
 <main class='mb-16 p-8'>
 	<Badge large href='https://svelte-5-preview.vercel.app/coverage/index.html'>Svete 5 Coverage report</Badge>
+	<Badge large color='yellow'>Flowbite Svelte: v{__FLOWBITE_SVELTE_VERSION__}</Badge>
+	<Badge large color='gray'>Svelte: v{__SVELTE_VERSION__}</Badge>
+	<Badge large color='green'>SvelteKit: v{__SVELTEKIT_VERSION__}</Badge>
+	<Badge large color='indigo'>Vite: v{__VITE_VERSION__}</Badge>
 	<slot></slot>
 </main>
 
