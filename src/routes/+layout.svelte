ed8630779d49c87847f0d8552d45079a26da760e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-07-01 16:59:55 +0200

    fix: add dark to body

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import { A, Badge } from 'flowbite-svelte';
 </script>
 
-<main class='mb-16 p-2 min-w-96 mx-auto'>
+<main class='mb-16 p-8 min-w-96 mx-auto'>
 	<div class='bg-white dark:bg-gray-900 p-2 flex justify-center gap-2'>
 		<Badge large href='https://svelte-5-preview.vercel.app/coverage/index.html'>Svete 5 Coverage report</Badge>
 		<Badge large color='yellow'>Flowbite Svelte: v{__FLOWBITE_SVELTE_VERSION__}</Badge>
